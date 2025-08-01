011e55e4f8076516e5ccb4c3df1b0abc4fad4317
jax <jax@apus.network>
2025-08-01 09:49:19 +0800

    cp server.cpp from llama.cpp

--- cmake/wasi_nn.cmake
+++ cmake/wasi_nn.cmake
@@ -6,6 +6,7 @@ add_library(
     wasi_nn_backend
     SHARED
         ${CMAKE_CURRENT_SOURCE_DIR}/src/wasi_nn_llama.cpp
+        ${CMAKE_CURRENT_SOURCE_DIR}/src/server/server.cpp
 )
 
 
@@ -13,6 +14,7 @@ target_include_directories(
     wasi_nn_backend
     PUBLIC
         $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/server>
         ${cjson_SOURCE_DIR}
 )
 
